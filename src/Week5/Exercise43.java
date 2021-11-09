6def32d8b3665d7266b2cdb204542e6e129c25d4
sebastienPelissier <sebastien.pelissier@zol.fr>
2021-11-09 19:10:37 +0100

    fix exercise43

--- src/Week5/Exercise43.java
+++ src/Week5/Exercise43.java
@@ -4,33 +4,37 @@ import java.util.Scanner;
 
 public class Exercise43 {
     public static void main(String[] args){
-        int i,length = 10, result = 0;
-        for(i=1; i<=length; i++ ){
-            result = result+i;
-        }
+        int length = 10;
+        double result = 0;
+        result = getResult(length);
         System.out.println(result);
 
-        int i2,length2 = 0, result2 = 0;
+        length = 0;
+        result = 0;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter a digit for length");
-        length2 = Integer.parseInt(scanner.nextLine());
+        length = Integer.parseInt(scanner.nextLine());
+        result = getResult(length);
+        System.out.println(result);
 
-        for(i2=1; i2<=length2; i2++ ){
-            result2 = result2+i2;
-        }
-        System.out.println(result2);
-
-        int i3;
-        double result3 = 0.0, length3 = 0.0;
+        length = 0;
+        result = 0.0;
         System.out.println("Please enter a digit for length");
-        length3 = Integer.parseInt(scanner.nextLine());
+        length = Integer.parseInt(scanner.nextLine());
+        result = getResult(length);
 
-        for(i3=1; i3<=length3; i3++ ){
-            result3 = result3+i3;
-        }
-        double response = result3 / length3;
+        double response = result / length;
         System.out.println(response);
         scanner.close();
-
     }
+
+    private static double getResult (Integer length){
+        double result = 0.0;
+        for(int i=0; i<length; i++ ){
+            result = result+i;
+        }
+
+        return result;
+    }
+
 }
