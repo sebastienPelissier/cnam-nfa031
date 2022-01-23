c437168ded3187dd0e225cec31f967afd74d0b59
sebastienPelissier <sebastien.pelissier@zol.fr>
2022-01-23 19:19:01 +0100

    WIP mastermind check exit

--- src/Mastermind/Mastermind.java
+++ src/Mastermind/Mastermind.java
@@ -1,14 +1,28 @@
 package Mastermind;
 
-import java.util.Locale;
 import java.util.Scanner;
 
 public class Mastermind {
+
+    static String [] TAB_REF_COLORS = {
+            "rouge",
+            "jaune",
+            "vert",
+            "bleu",
+            "orange",
+            "blanc",
+            "violet",
+            "fuchsia"
+    };
+
+    static int NB_COLORS = 4;
 
     public static void main(String [] arg)
     {
         Scanner scanner = new Scanner(System.in);
         String [] secretCombination = new String[0];
+        boolean result = false;
+
         int answer = 0;
         System.out.println("### WELCOME ###");
 
@@ -24,7 +38,17 @@ public class Mastermind {
             switch (answer){
                 case 1:
                     secretCombination = generateRandomCombination();
-                    playGame(secretCombination, scanner);
+                    result = playGame(secretCombination, scanner);
+
+//                    System.out.println(result);
+                    if (result){
+                        System.out.println("\n###############\n" +
+                                "### YOU WIN ###" +
+                                "\n###############\n"
+                        );
+                    } else {
+                        System.out.println("### YOU LOSE ###");
+                    }
                     break;
                 case 2:
                     showSolution(secretCombination);
@@ -44,21 +68,8 @@ public class Mastermind {
         scanner.close();
     }
 
-    static String [] TAB_REF_COLORS = {
-            "rouge",
-            "jaune",
-            "vert",
-            "bleu",
-            "orange",
-            "blanc",
-            "violet",
-            "fuchsia"
-    };
-
-    static int NB_COLORS = 4;
-
     // Generate random combination of 4 colors
-    static String [] generateRandomCombination() {
+    private static String [] generateRandomCombination() {
         String [] combination = new String[NB_COLORS];
         int currentPosition = 0;
         while(currentPosition!=NB_COLORS) {
@@ -72,21 +83,105 @@ public class Mastermind {
         return combination;
     }
 
-    static boolean playGame(String [] secretCombination, Scanner scanner)
+    private static boolean playGame(String [] secretCombination, Scanner scanner)
     {
         showSolution(secretCombination);
 
-        String [] userCombination = new String[secretCombination.length];
-        if (!(secretCombination.length > 0)){
-           System.out.println("Error, secret combination is empty");
+        boolean exit = false, response = false;
+        int maxTry = 2, totalTry = 0;
 
-           return false;
+        if (!(secretCombination.length > 0)){
+            System.out.println("Error, secret combination is empty");
+
+            return false;
         }
 
         System.out.println(
                 "Combination is generated\n" +
                 "Good Luck ! :)\n"
         );
+
+        do {
+            boolean result = isEndGame(secretCombination, scanner);
+            System.out.println(result);
+            totalTry ++;
+
+            System.out.println(totalTry);
+            System.out.println(totalTry == maxTry);
+
+
+            if (result){
+                exit = true;
+                response = true;
+
+            } else if (totalTry == maxTry) {
+                exit = true;
+            }
+
+        } while (!exit);
+
+       return response;
+
+    }
+
+    private static void showSolution(String [] secretCombination)
+    {
+        for(int i=0;i<4;i++) {
+            System.out.println(secretCombination[i]);
+        }
+        // jump to line
+        System.out.println("");
+    }
+
+    private static void showColorAuthorized()
+    {
+        for(int i=0;i<TAB_REF_COLORS.length;i++) {
+            System.out.println(TAB_REF_COLORS[i]);
+        }
+        // jump to line
+        System.out.println("");
+    }
+
+    private static boolean isIn(String iStringToFind, String [] iTab) {
+        int size = iTab.length;
+        for(int i=0;i<size;i++) {
+            if(iStringToFind.equalsIgnoreCase(iTab[i])) return true;
+        }
+        return false;
+    }
+
+    private static boolean checkUserResponse(String [] userCombination, String [] secretCombination) {
+        int goodColorAndPosition = 0;
+        int goodColorWrongPosition = 0;
+
+        for (int i = 0; i < userCombination.length; i++) {
+
+            System.out.printf("user %s, cpu %s\n", userCombination[i], secretCombination[i]);
+
+            if (userCombination[i].equals(secretCombination[i])) {
+                goodColorAndPosition++;
+                continue;
+            }
+
+            for (int j = 0; j < secretCombination.length; j++) {
+                if (userCombination[i].equals(secretCombination[j])) {
+                    goodColorWrongPosition++;
+                }
+            }
+        }
+
+        System.out.printf("goodColorAndPosition = %s\ngoodColorWrongPosition = %s\n", goodColorAndPosition, goodColorWrongPosition);
+
+        if (goodColorAndPosition < userCombination.length) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private static boolean isEndGame(String [] secretCombination, Scanner scanner )
+    {
+        String [] userCombination = new String[secretCombination.length];
 
         for(int i=0; i < userCombination.length; i++){
             String answer = "";
@@ -110,59 +205,6 @@ public class Mastermind {
             userCombination[i] = answer;
         }
 
-        checkUserResponse(userCombination, secretCombination);
-
-        return true;
+        return checkUserResponse(userCombination, secretCombination);
     }
-
-    static void showSolution(String [] secretCombination)
-    {
-        for(int i=0;i<4;i++) {
-            System.out.println(secretCombination[i]);
-        }
-        // jump to line
-        System.out.println("");
-    }
-
-    static void showColorAuthorized()
-    {
-        for(int i=0;i<TAB_REF_COLORS.length;i++) {
-            System.out.println(TAB_REF_COLORS[i]);
-        }
-        // jump to line
-        System.out.println("");
-    }
-
-    static boolean isIn(String iStringToFind, String [] iTab) {
-        int size = iTab.length;
-        for(int i=0;i<size;i++) {
-            if(iStringToFind.equalsIgnoreCase(iTab[i])) return true;
-        }
-        return false;
-    }
-
-    private static void checkUserResponse(String [] userCombination, String [] secretCombination)
-    {
-        int goodColorAndPosition = 0;
-        int goodColorWrongPosition = 0;
-
-        for(int i=0; i< userCombination.length; i++){
-
-            System.out.printf("user %s, cpu %s\n", userCombination[i], secretCombination[i]);
-
-            if (userCombination[i].equals(secretCombination[i])){
-                goodColorAndPosition ++;
-                continue;
-            }
-
-            for(int j=0; j< secretCombination.length; j++){
-                if (userCombination[i].equals(secretCombination[j])){
-                    goodColorWrongPosition++;
-                }
-            }
-        }
-
-        System.out.printf("goodColorAndPosition = %s\ngoodColorWrongPosition = %s\n", goodColorAndPosition, goodColorWrongPosition );
-    }
-
 }
